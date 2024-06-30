23baf5d210c7cc05f6d7089fb00ebe29c02d35a9
Delaneyrs <delaneyschlegel05@gmail.com>
2024-06-30 16:29:11 -0700

    a

--- ica/ica11/ica11.js
+++ ica/ica11/ica11.js
@@ -1,4 +1,47 @@
+alert("This is a message");
 
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", () =>
+    displayMessage("Your inbox is almost full — delete some mails", "warning"),
+  );
+  
+
+function displayMessage(msgText, msgType) {
+
+    const body = document.body;
+
+const panel = document.createElement("div");
+panel.setAttribute("class", "msgBox");
+body.appendChild(panel);
+
+const msg = document.createElement("p");
+msg.textContent = msgText;
+panel.appendChild(msg);
+
+const closeBtn = document.createElement("button");
+closeBtn.textContent = "x";
+panel.appendChild(closeBtn);
+
+closeBtn.addEventListener("click", () =>
+  panel.parentNode.removeChild(panel),
+);
+if (msgType === "warning") {
+    msg.style.backgroundImage = "url(icons/warning.png)";
+    panel.style.backgroundColor = "red";
+  } else if (msgType === "chat") {
+    msg.style.backgroundImage = "url(icons/chat.png)";
+    panel.style.backgroundColor = "aqua";
+  } else {
+    msg.style.paddingLeft = "20px";
+  }
+  }
+  
+ displayMessage();
+
+
+
+/*
 const displayBtn = document.getElementById("displayBtn");
 const messageBox = document.getElementById("messageBox");
 const messageText = document.getElementById("messageText");
@@ -31,3 +74,4 @@ closeBtn.addEventListener("click", function() {
 }
 );
 
+*/
