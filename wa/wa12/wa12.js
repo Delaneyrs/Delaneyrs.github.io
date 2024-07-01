dfe85f6d579a8842b9deb27445636d315aed9c05
Delaneyrs <delaneyschlegel05@gmail.com>
2024-06-30 21:44:32 -0700

    Update wa12.js

--- wa/wa12/wa12.js
+++ wa/wa12/wa12.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
         loadingElement.style.display = 'block';
         $.ajax({
             method: 'GET',
-            url: endpoint,
+            url:'https://icanhazdadjoke.com/',
             headers: {
                 'Accept': 'application/json',
                 'User-Agent': userAgent
